b3b64195add27c2e09830a217aab07fea6e9ece4
codewithsaidul <codewithsaidul@gmail.com>
2024-05-16 17:08:44 +0600

    jwt token added

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 require("dotenv").config();
 
 const app = express();
@@ -10,18 +12,30 @@ const corsOptions = {
   origin: [
     "http://localhost:5173",
     "http://localhost:4173",
-    "https://6644ee033bda8d4dd374a833--melodic-fudge-eaf185.netlify.app",
     "https://sakunest.netlify.app",
+    "https://sakunest-b052c.web.app",
   ],
   credentials: true,
   optionSuccessStatus: 200,
 };
 
+
+
 app.use(cors(corsOptions));
 app.use(express.json());
+app.use(cookieParser());
+
+
+const cookieOptions = {
+  httpOnly: true,
+  secure: process.env.NODE_ENV === "production",
+  sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+};
+//localhost:5000 and localhost:5173 are treated as same site.  so sameSite value must be strict in development server.  in production sameSite will be none
+// in development server secure will false .  in production secure will be true
+ 
 
 // MongoDb Connected
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.lggjuua.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -34,38 +48,50 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-
-   
     const roomsCollection = client.db("sakuNest").collection("rooms");
     const reviewsCollection = client.db("sakuNest").collection("reviews");
-    const bookingCollection = client.db('sakuNest').collection('bookings')
+    const bookingCollection = client.db("sakuNest").collection("bookings");
 
-    
+    // JWt Generate
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "365d",
+      });
+      res.cookie("token", token, cookieOptions).send({ success: true });
+    });
+
+    // clear token on logout
+    app.get("/logout", (req, res) => {
+      res
+        .clearCookie("token", {
+          httpOnly: true,
+          secure: process.env.NODE_ENV === "production",
+          sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+          maxAge: 0,
+        })
+        .send({ success: true });
+    });
+
     // Get The All Rooms Data From DB
     app.get("/rooms", async (req, res) => {
-      const sort = req.query.sort;
-      console.log(sort)
-      // const options = {};
-      // if (sort) options = { sort: { pricePerNight: sort === 'asc' ? 1 : -1 } }
       const result = await roomsCollection.find().toArray();
       res.send(result);
     });
 
+    // Get All Bookings
+    app.get("/bookings", async (req, res) => {
+      const result = await bookingCollection.find().toArray();
+      res.send(result);
+    });
 
-
-    
-      app.get('/bookings', async(req, res) => {
-        const result = await bookingCollection.find().toArray()
-        res.send(result)
-      })
-
-      app.get('/booking/:id', async(req, res) => {
-        const id = req.params.id
-        const query = { _id: new ObjectId(id)};
-        const result = await bookingCollection.findOne(query);
-        res.send(result)
-      })
-
+    // Get Booings By ID
+    app.get("/booking/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await bookingCollection.findOne(query);
+      res.send(result);
+    });
 
     // Get Single Room Data Using Id
     app.get("/rooms/:id", async (req, res) => {
@@ -75,94 +101,90 @@ async function run() {
       res.send(result);
     });
 
+    // Get Booing Data for specific user by email
+    app.get("/bookings/:email", async (req, res) => {
+    
+      const email = req.params.email;
 
-    app.get('/bookings/:email', async (req, res) => {
-      const email = req.params.email
-      const query = { email }
+      const query = { email };
       const result = await bookingCollection.find(query).toArray();
-      res.send(result)
-    })
+      res.send(result);
+    });
 
+    // Get All REview
     app.get("/review", async (req, res) => {
       const query = { time: -1 };
       const result = await reviewsCollection.find().sort(query).toArray();
       res.send(result);
     });
 
-
-    app.post('/reviews', async (req, res) => {
+    // Post a Review
+    app.post("/reviews", async (req, res) => {
       const review = req.body;
-      
-      const result =  await reviewsCollection.insertOne(review);
-      const reviewRoomName = review.roomName
-      const reviewCount = await reviewsCollection.countDocuments( { roomName: reviewRoomName})
+      const result = await reviewsCollection.insertOne(review);
+      const reviewRoomName = review.roomName;
+      const reviewCount = await reviewsCollection.countDocuments({
+        roomName: reviewRoomName,
+      });
       await roomsCollection.updateOne(
-        { roomName: reviewRoomName},
-        { $set: { reviewCount : reviewCount}}
-      )
+        { roomName: reviewRoomName },
+        { $set: { reviewCount: reviewCount } }
+      );
 
-     res.send(result)
+      res.send(result);
     });
 
-
-    app.post('/bookings', async (req, res) => {
+    // Request for booking
+    app.post("/bookings", async (req, res) => {
       const booking = req.body;
-      
-      const result =  await bookingCollection.insertOne(booking);
+
+      const result = await bookingCollection.insertOne(booking);
 
       await roomsCollection.updateOne(
-        { _id: new ObjectId(booking.roomId)},
-        { $set: { availability : false}}
-      )
+        { _id: new ObjectId(booking.roomId) },
+        { $set: { availability: false } }
+      );
 
-     res.send(result)
+      res.send(result);
     });
-
-
 
     // Update Booking Status
-    app.patch('/bookinges/:id', async (req, res) => {
+    app.patch("/bookinges/:id", async (req, res) => {
       const id = req.params.id;
-      const booking = req.body
-      console.log(id, booking.bookingDate)
-      const query = { _id: new ObjectId(id)}
+      const booking = req.body;
+      console.log(id, booking.bookingDate);
+      const query = { _id: new ObjectId(id) };
       const updateDoc = {
         $set: {
-          bookingDate: booking.bookingDate
-        }
-      }
+          bookingDate: booking.bookingDate,
+        },
+      };
       const result = await bookingCollection.updateOne(query, updateDoc);
 
-      res.send(result)
-      
-    })
-
+      res.send(result);
+    });
 
     // Update Booking Status
-    app.patch('/bookRoom/:id', async (req, res) => {
+    app.patch("/bookRoom/:id", async (req, res) => {
       const id = req.params.id;
-      const status = req.body
-      const query = { _id: new ObjectId(id)}
+      const status = req.body;
+      const query = { _id: new ObjectId(id) };
       const updateDoc = {
-        $set: status
-      }
+        $set: status,
+      };
       const result = await bookingCollection.updateOne(query, updateDoc);
 
       const cancelledBooking = await bookingCollection.findOne(query);
       const roomId = cancelledBooking.roomId;
 
-
       // Update Room Collection Availability
       await roomsCollection.updateOne(
-        { _id: new ObjectId(roomId)},
-        { $set: { availability: true }}
-      )
+        { _id: new ObjectId(roomId) },
+        { $set: { availability: true } }
+      );
 
-      res.send(result)
-      
-    })
-    
-
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
